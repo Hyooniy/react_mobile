36d42b54b74ab4de1398129ce351a72bb368f859
hyooniy <jkas8233@naver.com>
2023-08-11 17:37:34 +0900

    프로젝트 완성

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import {BiHome} from 'react-icons/bi';
+import {BsCart4} from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import {useSelector } from 'react-redux';
+
+export default function Cart(){
+  const {cartProductIds} = useSelector(state => state.cart)
+  return(
+    <nav>
+    <Link to='/'>
+    <img src='./images/starbucks.png' /> 
+  </Link>
+      <ul>
+        <li>
+          <Link to='/cart'>
+            <BsCart4 />
+            <span>{cartProductIds.length}</span>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
